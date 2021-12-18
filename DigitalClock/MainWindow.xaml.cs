95aa2dce57906182580b6e3224748b9b90e8995c
Mariusz 'Mario' Zajac <zajac@gmx.us>
2021-12-18 14:00:42 +0100

    Working clock

---
+++ DigitalClock/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace DigitalClock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval= TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_tick;
+            timer.Start();  
+        }
+
+        private void QuitButton_click1(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        public void Timer_tick(object sender, EventArgs e)
+        {
+            TimeLabel.Content = DateTime.Now.ToLongTimeString();
+            DateLabel.Content = DateTime.Now.ToLongDateString();
+
+        }
+    }
+}
