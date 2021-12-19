1b7b0835223fc8da9c2b5d67ac006291a2fe0102
Mariusz 'Mario' Zajac <zajac@gmx.us>
2021-12-19 10:23:18 +0100

    Create button need czas do odliczania pojawiający się po nacisnięciu przycisku

--- DigitalClock/MainWindow.xaml.cs
+++ DigitalClock/MainWindow.xaml.cs
@@ -1,6 +1,14 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Timers;
 using System.Windows;
 using System.Windows.Threading;
+using System.Windows.Forms;
+using static System.DateTime;
+using Timer = System.Windows.Forms.Timer;
+
 
 namespace DigitalClock
 {
@@ -17,21 +25,58 @@ namespace DigitalClock
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval= TimeSpan.FromSeconds(1);
+            timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += Timer_tick;
-            timer.Start();  
+            timer.Start();
+        }
+    
+        private void Focus_30_OnClick(object sender, EventArgs e)
+        {  
+            //TODO
+            // DateTime start = DateTime.Now;           
+            // Thread.Sleep(1000);
+            // DateTime end = DateTime.Now;
+            // TimeSpan timeDiff = end - start;
+            //
+            // DispatcherTimer timer1 = new DispatcherTimer();
+            // timer1.Interval = TimeSpan.FromSeconds(1);
+            // timer1.Tick += Timer_tick2;
+            // timer1.Start();
+            
+           //  Stopwatch stopwatch = new Stopwatch();
+           //  stopwatch.Start();
+           //
+           //  // ... This takes 10 seconds to finish.
+           //  for (int i = 0; i < 100; i++)
+           //  {
+           //     Thread.Sleep(1000);
+           //  }
+           //
+           //  // Stop.
+           //  stopwatch.Stop();
+           //
+           //  // Write hours, minutes and seconds.
+           // CountLabel.Content = stopwatch.Elapsed;
         }
 
         private void QuitButton_click1(object sender, RoutedEventArgs e)
         {
             Environment.Exit(1);
         }
-
         public void Timer_tick(object sender, EventArgs e)
         {
-            TimeLabel.Content = DateTime.Now.ToLongTimeString();
-            DateLabel.Content = DateTime.Now.ToLongDateString();
+            TimeLabel.Content = Now.ToLongTimeString();
+            DateLabel.Content = Now.ToLongDateString();
+        }
 
+        private void Break_5_OnClick(object sender, RoutedEventArgs e)
+        {
+            //TODO
+        }
+
+        private void Break_15_OnClick(object sender, RoutedEventArgs e)
+        {
+             //TODO
         }
     }
 }
